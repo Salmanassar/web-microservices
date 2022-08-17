9115a1433dae3756b7049cb44e39ac884c4a8ef0
Aleksandr <folknerolek@gmail.com>
2022-08-17 15:08:21 +0200

    create Pre filter and Post filter for handling the request

---
+++ ApiGateway/src/main/java/com/atos/photo/app/apigateway/filters/PostFilter.java
@@ -0,0 +1,21 @@
+package com.atos.photo.app.apigateway.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class PostFilter implements GlobalFilter {
+    private final Logger logger = LoggerFactory.getLogger(PostFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            logger.info("Global Post Filter executed ....");
+        }));
+    }
+}
---
+++ ApiGateway/src/main/java/com/atos/photo/app/apigateway/filters/PreFilter.java
@@ -0,0 +1,31 @@
+package com.atos.photo.app.apigateway.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Set;
+import java.util.function.Consumer;
+@Component
+public class PreFilter implements GlobalFilter {
+    final Logger logger = LoggerFactory.getLogger(PreFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("PreFilter is executed");
+        String getRequestPath = exchange.getRequest().getPath().toString();
+        logger.info("The request path is " + getRequestPath);
+       HttpHeaders headers  = exchange.getRequest().getHeaders();
+       Set<String> headersName = headers.keySet();
+       headersName.forEach(headerName->{
+           String headerValue = headers.getFirst(headerName);
+           logger.info("Header name " + headerValue);
+       });
+        return chain.filter(exchange);
+    }
+}

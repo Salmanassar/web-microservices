74610102b3e27ab638bd5a22f4556e269bbf2c60
salmanassar <folknerolek@gmail.com>
2022-08-26 10:14:54 +0200

    add microservice PhotoAppApiAlbums

---
+++ PhotoAppApiAlbums/src/test/java/com/appsdeveloperblog/photoapp/api/albums/PhotoAppApiAlbums/PhotoAppApiAlbumsApplicationTests.java
@@ -0,0 +1,19 @@
+package com.appsdeveloperblog.photoapp.api.albums.PhotoAppApiAlbums;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhotoAppApiAlbumsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+

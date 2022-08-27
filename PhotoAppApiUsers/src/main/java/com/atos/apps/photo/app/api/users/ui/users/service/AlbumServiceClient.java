79bfebcb6bbd7a90259015eb3f8a57cc4e7d7d80
salmanassar <folknerolek@gmail.com>
2022-08-28 00:46:21 +0200

    add Resilience4j-Retry and set up time and order for it and Circuit Breaker

--- PhotoAppApiUsers/src/main/java/com/atos/apps/photo/app/api/users/ui/users/service/AlbumServiceClient.java
+++ PhotoAppApiUsers/src/main/java/com/atos/apps/photo/app/api/users/ui/users/service/AlbumServiceClient.java
@@ -2,6 +2,7 @@ package com.atos.apps.photo.app.api.users.ui.users.service;
 
 import com.atos.apps.photo.app.api.users.ui.model.AlbumResponseModel;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ public interface AlbumServiceClient {
 
     @GetMapping(value = "/users/{id}/albums", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @Retry(name = "albums-ws")
     @CircuitBreaker(name = "albums-ws", fallbackMethod = "getAlbumsFallback")
     List<AlbumResponseModel> getAlbums(@PathVariable("id") final String id);
 

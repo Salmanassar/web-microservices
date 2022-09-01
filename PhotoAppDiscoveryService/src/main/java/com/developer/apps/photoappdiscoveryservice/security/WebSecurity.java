a7810adb969526873f63e5aa58aa1737cf64f92d
salmanassar <folknerolek@gmail.com>
2022-09-02 01:42:25 +0200

    delete unsupported references

--- PhotoAppDiscoveryService/src/main/java/com/developer/apps/photoappdiscoveryservice/security/WebSecurity.java
+++ PhotoAppDiscoveryService/src/main/java/com/developer/apps/photoappdiscoveryservice/security/WebSecurity.java
@@ -1,10 +1,6 @@
 package com.developer.apps.photoappdiscoveryservice.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;

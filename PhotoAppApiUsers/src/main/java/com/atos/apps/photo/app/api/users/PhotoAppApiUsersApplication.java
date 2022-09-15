fd96cd10c40ae386d1b9891e17d2033d1c6da8c4
salmanassar <folknerolek@gmail.com>
2022-09-15 17:48:39 +0200

    create additional settings for other types production profile and development profile

--- PhotoAppApiUsers/src/main/java/com/atos/apps/photo/app/api/users/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/atos/apps/photo/app/api/users/PhotoAppApiUsersApplication.java
@@ -1,12 +1,15 @@
 package com.atos.apps.photo.app.api.users;
 
 import feign.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,12 +18,19 @@ import org.springframework.web.client.RestTemplate;
 @EnableFeignClients
 public class PhotoAppApiUsersApplication {
 
+    private final Environment environment;
+
+    @Autowired
+    public PhotoAppApiUsersApplication(Environment environment) {
+        this.environment = environment;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(PhotoAppApiUsersApplication.class, args);
     }
 
     @Bean
-    public BCryptPasswordEncoder getBCryptPasswordEncoder(){
+    public BCryptPasswordEncoder getBCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
@@ -31,7 +41,34 @@ public class PhotoAppApiUsersApplication {
     }
 
     @Bean
-    public Logger.Level feignLogger(){
+    public Logger.Level feignLogger() {
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    @Profile("production")
+    public Logger.Level feignProductionLogger() {
+        return Logger.Level.BASIC;
+    }
+
+    @Bean
+    @Profile("production")
+    public String createProductionEnvironmentProperties() {
+        System.out.println("Production bean created " + environment.getProperty("myapplication.environment"));
+        return "production environment properties";
+    }
+
+    @Bean
+    @Profile("!production")
+    public String createNotProductionEnvironmentProperties() {
+        System.out.println("Not Production bean created " + environment.getProperty("myapplication.environment"));
+        return "Not production environment properties";
+    }
+
+    @Bean
+    @Profile("development")
+    public String createDevelopmentEnvironmentProperties() {
+        System.out.println("Development bean created " + environment.getProperty("myapplication.environment"));
+        return "development environment properties";
     }
 }
